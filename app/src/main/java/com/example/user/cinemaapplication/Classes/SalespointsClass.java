da13f45c508523adca6979b9438d77967cd299a6
Alex Tkachev <Sasatkwot2@gmail.com>
2018-05-09 16:51:00 +0300

    added glasses and salespoints class
    dd

---
+++ app/src/main/java/com/example/user/cinemaapplication/Classes/SalespointsClass.java
@@ -0,0 +1,85 @@
+package com.example.user.cinemaapplication.Classes;
+
+public class SalespointsClass {
+    int id;
+    int theater;
+    int saleszone;
+    int type;
+    String name;
+    String acronym;
+    int company;
+    String logo;
+    String color;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTheater() {
+        return theater;
+    }
+
+    public void setTheater(int theater) {
+        this.theater = theater;
+    }
+
+    public int getSaleszone() {
+        return saleszone;
+    }
+
+    public void setSaleszone(int saleszone) {
+        this.saleszone = saleszone;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAcronym() {
+        return acronym;
+    }
+
+    public void setAcronym(String acronym) {
+        this.acronym = acronym;
+    }
+
+    public int getCompany() {
+        return company;
+    }
+
+    public void setCompany(int company) {
+        this.company = company;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
